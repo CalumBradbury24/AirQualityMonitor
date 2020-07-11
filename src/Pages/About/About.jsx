ab36cc96048d716f833dcf52456fd4f9472f00b2
Calum Bradbury <eeycb2@nottingham.ac.uk>
2020-07-11 22:15:22 +0100

    added information to about page, implemented historical graphs

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,25 +4,28 @@ import "./About.styles.scss";
 const About = () => {
   return (
     <div className="about-page">
-      <h2>Project details</h2>
-      <p>
+      <h2 className = 'titles'>Project details</h2>
+      <p className = 'paragraph'>
         The sensing system built for this project consists of a Plantower
         PMS5003 Particulate Matter Sensor that can be found{" "}
         <a href="https://shop.pimoroni.com/products/pms5003-particulate-matter-sensor-with-cable#show-accessories">
           here
-        </a> 
-        .</p>
-        <p>
-        A Raspberry Pi 3B+ interfaces with the particulate matter sensor
-        and, using the PMS5003 Python library, posts recorded particulate matter, temperature and humidity data to a NoSQL MongoDB
-        database.
+        </a>
+        .
       </p>
-      <p>
-       This web application, built using React.js, HTML5, SASS for the front-end and Node.js and express.js for the backend, then displays and analyses the previously recorded data. 
+      <p className = 'paragraph'>
+        A Raspberry Pi 3B+ interfaces with the particulate matter sensor and,
+        using the PMS5003 Python library, posts recorded particulate matter,
+        temperature and humidity data to a NoSQL MongoDB database.
       </p>
-      <h2>Additional Information</h2>
-      <h3>What is Particulate Matter?</h3>
-      <p>
+      <p className = 'paragraph'>
+        This web application, built using React.js, HTML5, SASS for the
+        front-end and Node.js and express.js for the backend, then displays and
+        analyses the previously recorded data.
+      </p>
+      <h2 className = 'titles'>Additional Information</h2>
+      <h3 className = 'titles'>What is Particulate Matter?</h3>
+      <p className = 'paragraph'>
         Particulate matter is the sum of all solid and liquid particles
         suspended in air many of which are hazardous. This complex mixture
         includes both organic and inorganic particles, such as dust, pollen,
@@ -32,10 +35,30 @@ const About = () => {
         indirectly formed, when gaseous pollutants previously emitted to air
         turn into particulate matter.
       </p>
-      <h3>How does Particulate Matter affect humans?</h3>
-      <h3>How are humans exposed to Particulate Matter?</h3>
-      <h3>Why does particle size matter?</h3>
-      <h3>What is the Daily Air Quality Index?</h3>
+      <h3 className = 'titles'>How does Particulate Matter affect humans?</h3>
+      <p className = 'paragraph'>
+        The World Health Organisation (WHO) state that Long-term exposure to
+        current ambient PM concentrations may lead to a marked reduction in life
+        expectancy. The reduction in life expectancy is primarily due to
+        increased cardio-pulmonary and lung cancer mortality. Increases are
+        likely in lower respiratory symptoms and reduced lung function in
+        children, and chronic obstructive pulmonary disease and reduced lung
+        function in adults. See https://www.euro.who.int/__data/assets/pdf_file/0005/112199/E79097.pdf.
+      </p>
+      <h3 className = 'titles'>Why does particle size matter?</h3>
+      <p className = 'paragraph'>
+        The aerodynamic and physical properties of particles determine how they
+        are transported in air and how they can be removed from it. These
+        properties also govern how far they can travel into the air passages of
+        the respiratory system.
+      </p>
+      <h3 className = 'titles'>What is the Air Quality Index?</h3>
+      <p className = 'paragraph'>
+        The Air Quality Index (AQI) gives information about the levels of air
+        pollution. Numbered 1-10 and divided into four bands, low (1) to very
+        high (10), the index allows for the provision of air pollution levels in a
+        simple way.
+      </p>
     </div>
   );
 };
--- src/Pages/HistoricalReadings/HistoricalReadings.jsx
+++ src/Pages/HistoricalReadings/HistoricalReadings.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import axios from "axios";
 import Spinner from "../../Components/spinner/spinner";
-import RenderHistoricalReadings from '../../Components/renderHistoricalReadings/renderHistoricalReadings';
-import './HistoricalReadings.styles.scss';
-
+import { Line } from "react-chartjs-2";
+import RenderHistoricalReadings from "../../Components/renderHistoricalReadings/renderHistoricalReadings";
+import "./HistoricalReadings.styles.scss";
 
 class HistoricalReadings extends React.Component {
   state = {
     readings: [], //Hold readings from MongoDB
     dataLoaded: false, //Is the data loaded?
+    graphOption: "PM1", //Graph selection
   };
-
 
   componentDidMount = () => {
     axios
       .get("https://mysterious-sierra-11255.herokuapp.com/") //The port the backend server sits on
       .then((response) => {
         this.setState({ readings: response.data, dataLoaded: true });
-       // console.log(this.state.readings[0]);
       })
       .catch((error) => {
         console.log(error);
@@ -26,34 +25,363 @@ class HistoricalReadings extends React.Component {
 
   renderHistReadings = () => {
     const { readings } = this.state;
-   // return readings.map((reading) => {
-      return (
-        <RenderHistoricalReadings
-          reading={readings}
-        // key={reading._id}
-        />
-      );
-   // });
+    return <RenderHistoricalReadings reading={readings} />;
   };
 
-render() {
-  const { dataLoaded } = this.state;
-  return !dataLoaded === true ? (
-    <div className="loading">
-      <p className="loading-message">
-        <strong>Loading data from server...</strong>
-      </p>
-      <div className="spinner">
-        <Spinner />
+  getPM1LineData = () => {
+    const { readings } = this.state;
+    readings.reverse();
+    return {
+      labels: [
+        readings[6].date.substring(5, 10) +
+          "-" +
+          readings[6].date.substring(0, 2) +
+          " @ " +
+          readings[6].date.substring(11, 16),
+        readings[5].date.substring(5, 10) +
+          "-" +
+          readings[5].date.substring(0, 2) +
+          " @ " +
+          readings[5].date.substring(11, 16),
+        readings[4].date.substring(5, 10) +
+          "-" +
+          readings[4].date.substring(0, 2) +
+          " @ " +
+          readings[4].date.substring(11, 16),
+        readings[3].date.substring(5, 10) +
+          "-" +
+          readings[3].date.substring(0, 2) +
+          " @ " +
+          readings[3].date.substring(11, 16),
+        readings[2].date.substring(5, 10) +
+          "-" +
+          readings[2].date.substring(0, 2) +
+          " @ " +
+          readings[2].date.substring(11, 16),
+        readings[1].date.substring(5, 10) +
+          "-" +
+          readings[1].date.substring(0, 2) +
+          " @ " +
+          readings[1].date.substring(11, 16),
+        readings[0].date.substring(5, 10) +
+          "-" +
+          readings[0].date.substring(0, 2) +
+          " @ " +
+          readings[0].date.substring(11, 16),
+      ],
+      datasets: [
+        {
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "blue",
+          borderWidth: 1,
+          data: [
+            readings[6].PMOne,
+            readings[5].PMOne,
+            readings[4].PMOne,
+            readings[3].PMOne,
+            readings[2].PMOne,
+            readings[1].PMOne,
+            readings[0].PMOne,
+          ],
+        },
+      ],
+    };
+  };
+  getPM2LineData = () => {
+    const { readings } = this.state;
+    readings.reverse();
+    return {
+      labels: [
+        readings[6].date.substring(5, 10) +
+          "-" +
+          readings[6].date.substring(0, 2) +
+          " @ " +
+          readings[6].date.substring(11, 16),
+        readings[5].date.substring(5, 10) +
+          "-" +
+          readings[5].date.substring(0, 2) +
+          " @ " +
+          readings[5].date.substring(11, 16),
+        readings[4].date.substring(5, 10) +
+          "-" +
+          readings[4].date.substring(0, 2) +
+          " @ " +
+          readings[4].date.substring(11, 16),
+        readings[3].date.substring(5, 10) +
+          "-" +
+          readings[3].date.substring(0, 2) +
+          " @ " +
+          readings[3].date.substring(11, 16),
+        readings[2].date.substring(5, 10) +
+          "-" +
+          readings[2].date.substring(0, 2) +
+          " @ " +
+          readings[2].date.substring(11, 16),
+        readings[1].date.substring(5, 10) +
+          "-" +
+          readings[1].date.substring(0, 2) +
+          " @ " +
+          readings[1].date.substring(11, 16),
+        readings[0].date.substring(5, 10) +
+          "-" +
+          readings[0].date.substring(0, 2) +
+          " @ " +
+          readings[0].date.substring(11, 16),
+      ],
+      datasets: [
+        {
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "blue",
+          borderWidth: 1,
+          data: [
+            readings[6].PMTwoFive,
+            readings[5].PMTwoFive,
+            readings[4].PMTwoFive,
+            readings[3].PMTwoFive,
+            readings[2].PMTwoFive,
+            readings[1].PMTwoFive,
+            readings[0].PMTwoFive,
+          ],
+        },
+      ],
+    };
+  };
+  getPM10LineData = () => {
+    const { readings } = this.state;
+    readings.reverse();
+    var data =[];
+    var decrement = 6;
+    for(var i = 0; i<7; i++){//7 readings to display
+      data[i] =  readings[decrement].PMTen
+      decrement--;
+    }
+    return {
+      labels: [
+        readings[6].date.substring(5, 10) +
+          "-" +
+          readings[6].date.substring(0, 2) +
+          " @ " +
+          readings[6].date.substring(11, 16),
+        readings[5].date.substring(5, 10) +
+          "-" +
+          readings[5].date.substring(0, 2) +
+          " @ " +
+          readings[5].date.substring(11, 16),
+        readings[4].date.substring(5, 10) +
+          "-" +
+          readings[4].date.substring(0, 2) +
+          " @ " +
+          readings[4].date.substring(11, 16),
+        readings[3].date.substring(5, 10) +
+          "-" +
+          readings[3].date.substring(0, 2) +
+          " @ " +
+          readings[3].date.substring(11, 16),
+        readings[2].date.substring(5, 10) +
+          "-" +
+          readings[2].date.substring(0, 2) +
+          " @ " +
+          readings[2].date.substring(11, 16),
+        readings[1].date.substring(5, 10) +
+          "-" +
+          readings[1].date.substring(0, 2) +
+          " @ " +
+          readings[1].date.substring(11, 16),
+        readings[0].date.substring(5, 10) +
+          "-" +
+          readings[0].date.substring(0, 2) +
+          " @ " +
+          readings[0].date.substring(11, 16),
+      ],
+      datasets: [
+        {
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "blue",
+          borderWidth: 1,
+          data: data
+        },
+      ],
+    };
+  };
+
+  handleClick = (event) => () => {
+    this.setState({ graphOption: event });
+    console.log(event);
+  };
+
+  render() {
+    const { dataLoaded, graphOption } = this.state;
+    return !dataLoaded === true ? (
+      <div className="loading">
+        <p className="loading-message">
+          <strong>Loading data from server...</strong>
+        </p>
+        <div className="spinner">
+          <Spinner />
+        </div>
       </div>
-    </div>
-  ) : (
-    <div className = 'Historical-page '>
-      {this.renderHistReadings()}
-    </div>
-  );
+    ) : (
+      <div className="Historical-page ">
+        {this.renderHistReadings()}
+        <div className="graph-selector">
+          <h4
+            className={
+              "on-hover " + (graphOption === "PM1" ? "orange-red" : "default")
+            }
+            onClick={this.handleClick("PM1")}
+          >
+            PM<sub>1.0</sub>
+          </h4>
+          <h4
+            className={
+              "on-hover " + (graphOption === "PM2" ? "orange-red" : "default")
+            }
+            onClick={this.handleClick("PM2")}
+          >
+            PM<sub>2.5</sub>
+          </h4>
+          <h4
+            className={
+              "on-hover " + (graphOption === "PM10" ? "orange-red" : "default")
+            }
+            onClick={this.handleClick("PM10")}
+          >
+            PM<sub>10</sub>
+          </h4>
+        </div>
+        <div className="line-container">
+          {graphOption === "PM1" ? (
+            <div className = 'hist-graph-container'>
+              <Line
+                data={this.getPM1LineData()}
+                options={{
+                  responsive: true,
+                  aspectRatio: 1,
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: false,
+                  },
+                  scales: {
+                    xAxes: [
+                      {
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Date (DD-MM-YY)",
+                        },
+                        ticks: {
+                          maxRotation: 45,
+                          minRotation: 45,
+                          //padding: 0
+                        },
+                      },
+                    ],
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Concentration (ug/m3)",
+                        },
+                      },
+                    ],
+                  },
+                }}
+              />
+            </div>
+          ) : graphOption === "PM2" ? (
+            <div className='hist-graph-container'>
+              {" "}
+              <Line
+                data={this.getPM2LineData()}
+                options={{
+                  responsive: true,
+                  aspectRatio: 1,
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: false,
+                  },
+                  scales: {
+                    xAxes: [
+                      {
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Date (DD-MM-YY)",
+                        },
+                        ticks: {
+                          maxRotation: 45,
+                          minRotation: 45,
+                        },
+                      },
+                    ],
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Concentration (ug/m3)",
+                        },
+                      },
+                    ],
+                  },
+                }}
+              />
+            </div>
+          ) : (
+            <div className='hist-graph-container'>
+              {" "}
+              <Line
+                data={this.getPM10LineData()}
+                options={{
+                  responsive: true,
+                  aspectRatio: 1,
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: false,
+                  },
+                  scales: {
+                    xAxes: [
+                      {
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Date (DD-MM-YY)",
+                        },
+                        ticks: {
+                          maxRotation: 45,
+                          minRotation: 45,
+                        },
+                      },
+                    ],
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                        scaleLabel: {
+                          display: true,
+                          labelString: "Concentration (ug/m3)",
+                         
+                        },
+                      },
+                    ],
+                  },
+                }}
+              />{" "}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
- 
-};
 
 export default HistoricalReadings;
